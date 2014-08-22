cfa4cf35c7ef5af02e6913c6b3946d42e8fc7dd5
Diane Douglas <diane.douglas1@gmail.com>
2014-08-22 16:14:35 -0700

    WIP add UI edit methods.

--- expense_tracker_ui.rb
+++ expense_tracker_ui.rb
@@ -77,7 +77,7 @@ def add_category
 end
 
 def print_expense
-  puts "\n#{@current_expense.description}"
+  puts "#{@current_expense.description}"
   puts "Purchased on: #{@current_expense.date}"
   puts "$#{@current_expense.amount}"
 end
@@ -85,10 +85,12 @@ end
 def list_expenses
   if Expense.all.length == 0
     puts "You have not logged any expenses yet."
+    @current_expense = nil
   else
     puts "\nHere are all your logged expenses.\n"
-    Expense.all.each do |expense|
+    Expense.all.each_with_index do |expense, i|
       @current_expense = expense
+      puts "Expense #" + i.to_s
       print_expense
     end
   end
@@ -97,6 +99,7 @@ end
 def list_categories
   if Category.all.length == 0
     puts "You have not created any categories yet."
+    @current_category = nil
   else
     puts "Here are all the categories you have for your expenses."
     Category.all.each do |category|
@@ -105,4 +108,91 @@ def list_categories
   end
 end
 
+def select_expense
+  list_expenses
+  if @current_expense != nil
+    puts "Choose an expense by typing its number."
+    expense = gets.chomp.to_i
+    if
+
+  else
+    puts "You need to add some expenses before you can edit them."
+    main_menu
+  end
+end
+
+def select_category
+
+end
+
+def edit_expense
+  select_expense
+  choice = nil
+  until choice == 'M'
+    puts "Press [N] to edit the name of the expense."
+    puts "Press [P] to edit the price of the expense."
+    puts "Press [D] to edit the date of purchase."
+    puts "Press [R] to remove the purchase."
+    puts "Press [M] to return to the main menu."
+    choice = gets.chomp.upcase
+    case choice
+    when 'N'
+      puts "Enter new name:"
+      name = gets.chomp
+      @current_expense.update_description(name)
+      puts "OK, here ya go: "
+      print_expense
+    when 'P'
+      puts "Enter new price:"
+      price = gets.chomp
+      @current_expense.update_amount(price)
+      puts "OK, here ya go: "
+      print_expense
+    when 'D'
+      puts "Enter new date:"
+      date = gets.chomp
+      @current_expense.update_date(date)
+      puts "OK, here ya go: "
+      print_expense
+    when 'R'
+      puts "It's gone! Here are your other expenses."
+      @current_expense.delete
+      list_expenses
+    when 'M'
+      main_menu
+    else
+      puts "That's not an option, silly!\n\n"
+    end
+  end
+
+end
+
+def edit_category
+  select_category
+  choice = nil
+  until choice == 'M'
+    puts "Press [N] to edit the name of the category."
+    puts "Press [R] to remove the category."
+    puts "Press [M] to return to the main menu."
+    choice = gets.chomp.upcase
+    case choice
+    when 'N'
+      puts "Enter new name:"
+      name = gets.chomp
+      @current_expense.update_description(name)
+      puts "OK, here ya go: "
+      puts "#{@current_category.description}"
+    when 'R'
+      puts "It's gone! Here are your other expenses."
+      @current_category.delete
+      list_categories
+    when 'M'
+      main_menu
+    else
+      puts "That's not an option, silly!\n\n"
+    end
+  end
+end
+
 welcome
+#dollar sign, date formatting
